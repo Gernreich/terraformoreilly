d3120ccda53d85fee3d17870f48680fc5975d098
Michael Carnright <michael.carnright@sony.com>
2025-03-10 13:03:31 -0700

    asdf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+	region = "us-east-1"
+}
+resource "aws_instance" "example" {
+  ami = "ami-08b5b3a93ed654d19"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-carnright"
+  }
+}
